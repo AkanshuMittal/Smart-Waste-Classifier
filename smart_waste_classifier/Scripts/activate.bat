8043c457f530741d8ad0a72abdd4a0db0c0f0862
AkanshuMittal <akanshumittal150@gmail.com>
2025-05-20 17:15:42 +0530

    Add all files - Smart Waste Classifier

--- smart_waste_classifier/Scripts/activate.bat
+++ smart_waste_classifier/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\akans\OneDrive\Desktop\Smart Waste Classifier\smart_waste_classifier
+set VIRTUAL_ENV=C:\Smart Waste Classifier\smart_waste_classifier
 
 if not defined PROMPT set PROMPT=$P$G
 
